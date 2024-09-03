09142e006ab856f552e418937ac63954fad6402c
kurukshetra <kurukshetracodingschool@gmail.com>
2024-09-03 14:49:01 +0530

    Complete project

--- src/canvas.jsx
+++ src/canvas.jsx
@@ -4,7 +4,7 @@ import { Environment, ScrollControls } from '@react-three/drei';
 import MacContainer from './MacContainer'
 const canvas = () => {
   return (
-    <Canvas camera={{fav:20, position:[0 , 1, 50]}} >
+    <Canvas camera={{fav:20, position:[0 , 1, 60]}} >
     <Environment files={["https://dl.polyhaven.org/file/ph-assets/HDRIs/exr/4k/studio_small_08_4k.exr"]} />
     <ScrollControls pages={3}>
     <MacContainer />
